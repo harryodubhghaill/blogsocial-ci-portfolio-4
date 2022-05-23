38bc6c8881b28d4e23ea6bd83b8bce68bbed9324
harryodubhghaill <harryodubhghaill@hotmail.com>
2022-05-23 02:32:52 +0000

    update admin page

--- blog/admin.py
+++ blog/admin.py
@@ -5,8 +5,18 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug', 'community', 'created_on')
+    search_fields = ('title', 'content')
+    list_filter = ('created_on',)
     summernote_fields = ('body')
 
-admin.site.register(Community)
+@admin.register(Community)
+class CommuityAdmin(admin.ModelAdmin):
 
-admin.site.register(Comment)
+    list_display = ('name',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('author', 'created_on', 'post', 'body')
