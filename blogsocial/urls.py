b61fb4a25ce70c3de3ff75787dd34f85ae84058f
harryodubhghaill <harryodubhghaill@hotmail.com>
2022-05-23 03:50:30 +0000

    create home_screen_view and update urls

--- blogsocial/urls.py
+++ blogsocial/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from blog.views import (
+    home_screen_view,
+)
+
 urlpatterns = [
+    path('', home_screen_view, name='home'),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls'))
 ]
