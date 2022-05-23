503096bacaac5c5e7fb97f6e52565a3954154b41
harryodubhghaill <harryodubhghaill@hotmail.com>
2022-05-23 02:20:13 +0000

    install summernote & setup initial admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post, Community, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('body')
+
+admin.site.register(Community)
+
+admin.site.register(Comment)
