9e56f7a403603c94705318a6931f7161c33b1209
harryodubhghaill <harryodubhghaill@hotmail.com>
2022-05-23 02:00:44 +0000

    create initial blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.users import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Community(models.Model):
+
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE)
+    slug = models.CharField(max_length=200, unique=True)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=1000)
+    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"{self.author} commented on {self.post}"
+
+    def number_of_likes(self):
+        return self.likes.count()
