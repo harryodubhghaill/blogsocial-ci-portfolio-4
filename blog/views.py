9ccc3a2d7901bf5ff5a027d6b374fecd23abee0d
harryodubhghaill <harryodubhghaill@hotmail.com>
2022-05-25 14:01:37 +0000

    add home screen class based view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 
-def home_screen_view(request):
-
-    return render(request, "base.html", {})
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.order_by('-created_on')
+    template_name = 'post_list.html'
+    paginate_by = 12
