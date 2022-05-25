e237bb39b1cfea9e2c495907f0aebe2a5e6142c9
harryodubhghaill <harryodubhghaill@hotmail.com>
2022-05-25 18:28:22 +0000

    fix typo in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>', views.PostLikes.as_view('post_like')),
+    path('like/<slug:slug>', views.PostLikes.as_view(), name='post_like'),
 ]
