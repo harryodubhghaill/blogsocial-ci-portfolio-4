23004bfd300c8645d50fb7df1046bcc69e88b9b8
harryodubhghaill <harryodubhghaill@hotmail.com>
2022-05-25 22:40:59 +0000

    bug: reverse for post detail not found

--- blog/forms.py
+++ blog/forms.py
@@ -6,9 +6,3 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
-
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ('title', 'body', 'community', 'image',)
