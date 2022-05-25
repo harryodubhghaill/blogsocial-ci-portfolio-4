8903e629a229b532b8e9c248f8875365d7c785cf
harryodubhghaill <harryodubhghaill@hotmail.com>
2022-05-25 17:33:59 +0000

    update settings.py

--- blogsocial/settings.py
+++ blogsocial/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 
 if os.path.isfile("env.py"):
-   import env
+    import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,16 +52,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
 
+# allauth settings
 SITE_ID = 1
-
 LOGIN_REDIRECT_URL = '/'
-
 LOGOUT_REDIRECT_URL = '/'
-
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+# crispy forms settings
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 MIDDLEWARE = [
